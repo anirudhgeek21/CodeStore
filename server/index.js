f23bec76127d3dddf37b5f773ff8c223a80976e1
Anirudh Prajapati <81011908+anirudhgeek21@users.noreply.github.com>
2024-02-19 20:52:53 +0530

    Update index.js cors frontend integration

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ["https://code-store-frontend-one.vercel.app"],
     methods: ["POST","GET"],
     credentials: true
   }));
